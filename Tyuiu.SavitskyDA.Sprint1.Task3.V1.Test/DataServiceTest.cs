b452ffe9d8dee85ad8e5fb92a792a87e8793a7ce
Tyuiu.SavitskyDA@outlook.com <Tyuiu.SavitskyDA@outlook.com>
2023-09-22 23:03:51 +0500

    Task3

---
+++ Tyuiu.SavitskyDA.Sprint1.Task3.V1.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SavitskyDA.Sprint1.Task3.V1.Lib;
+
+namespace Tyuiu.SavitskyDA.Sprint1.Task3.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExspression()
+        {
+            //Объявите необходимые переменные и напишите программу вычисления объема цилиндра, предполагающий ввод исходных данных.
+            DataService ds = new DataService();
+            double r = 5;
+            double h = 11;
+            double wait = 863.5;
+            var res = ds.CylinderVolume(r, h);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SavitskyDA.Sprint1.Task3.V1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.SavitskyDA.Sprint1.Task3.V1.Lib;
+
+namespace Tyuiu.SavitskyDA.Sprint1.Task3.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Савицкий Д. А. | АСОиУб-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы с С#                                        *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Савицкий Данил Александрович | АСОиУб-23-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет по формуле p * r^2 * h  результат печатает его на экран       *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double r, h;
+
+            Console.WriteLine("Введите значение Радиус :");
+            r = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Высота :");
+            h = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.CylinderVolume(r, h) + " Объем цилиндра");
+
+            Console.ReadLine();
+        }
+    }
+}
